40255ebc4a721fe5b816da90add28bb807c37214
Adrián Antón <adrtler@gmail.com>
2021-11-16 18:12:57 +0100

    Creando el store de la aplicación

--- App.tsx
+++ App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import {Text, View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {Navigation} from './src/navigation/Navigation';
+import {Provider} from 'react-redux';
+import {store} from './src/redux/store';
 
 export const App = () => {
   return (
-    <NavigationContainer>
-      <Navigation />
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Navigation />
+      </NavigationContainer>
+    </Provider>
   );
 };
 
